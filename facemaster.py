bc8c7ce4c99afacf2f56a7eaabfd213f846e7b47
Amal Joseph <amaljoseph13@gmail.com>
2019-02-25 21:56:34 +0530

    Update facemaster.py

--- facemaster.py
+++ facemaster.py
@@ -8,13 +8,7 @@ label = []
 def predict(test_img):
 	img = cv2.imread(test_img).copy()
 	print "\n\n\n"
-	print "Face Prediction Running -\-"
-	face, rect, length = face_detect.face_detect(test_img)
-	print len(face), "faces detected."
-	for i in range(0, len(face)):
-		labeltemp, confidence = face_recognizer.predict(face[i])
-		label.append(labeltemp)
-	return img, label
+
 
 faces, labels = prepare_training_data.prepare_training_data("training-data")
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
